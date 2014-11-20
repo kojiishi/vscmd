3ef911929d0cbfe9daad2a2bbe94fa385a380aaa
Koji Ishii <kojiishi@hotmail.com>
2014-11-21 00:44:47 +0900

    Initial code

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vscmd
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                OpenFiles(args);
+            }
+            catch (Exception err)
+            {
+                Console.Error.WriteLine(err.ToString());
+            }
+        }
+
+        static void OpenFiles(string[] files)
+        {
+            dynamic dte = Marshal.GetActiveObject("VisualStudio.DTE.12.0");
+
+            foreach (var item in files)
+            {
+                var path = Path.GetFullPath(item);
+                dte.ExecuteCommand("File.OpenFile", "\"" + path + "\"");
+            }
+        }
+    }
+}
